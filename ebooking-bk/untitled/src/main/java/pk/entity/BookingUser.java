1e8a0647922769ff051f8bc28753d39fc598f839
petrkopecky <petr.kopecky@gmail.com>
2024-01-10 17:57:16 +0100

    01

---
+++ ebooking-bk/untitled/src/main/java/pk/entity/BookingUser.java
@@ -0,0 +1,89 @@
+package pk.entity;
+import jakarta.persistence.*;
+@Entity
+public class BookingUser {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String telefonNumber;
+    private String passwordHash;
+    private String userRole;
+    private boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefonNumber() {
+        return telefonNumber;
+    }
+
+    public void setTelefonNumber(String telefonNumber) {
+        this.telefonNumber = telefonNumber;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
