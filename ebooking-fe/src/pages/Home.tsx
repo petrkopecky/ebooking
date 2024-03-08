7f6e66b93b527354f2fa124130dcbe9c3799a1d0
petrkopecky <petr.kopecky@gmail.com>
2024-03-08 15:42:44 +0100

    u

--- ebooking-fe/src/pages/Home.tsx
+++ ebooking-fe/src/pages/Home.tsx
@@ -2,12 +2,33 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext, UserContextType, useUserContext } from "../UserContext";
 import Booking from "../components/Booking/Booking";
+import "./Home.css";
 
 const Home = () => {
   const userContext = useUserContext();
   const [render, setRender] = useState<Boolean>(true);
   return (
     <div>
+      <div className="home-nav-bar">
+        <p>Here is another paragraph.</p>
+        <div>
+          <p>another p</p>
+        </div>
+        <ul>
+          <li>
+            <a href="default.asp">Home</a>
+          </li>
+          <li>
+            <a href="news.asp">News</a>
+          </li>
+          <li>
+            <a href="contact.asp">Contact</a>
+          </li>
+          <li>
+            <a href="about.asp">About</a>
+          </li>
+        </ul>
+      </div>
       <p>Home</p>
       <p>{userContext?.bookingUser?.userName}</p>
       <p>{userContext?.bookingUser?.userRole}</p>
